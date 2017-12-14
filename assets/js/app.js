9a27ecb04148e41019ef19403ea79288aa8ef590
vpsa23 <vpsa23@hotmail.com>
2017-12-14 11:30:39 -0300

    habilitando botones

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,6 @@ $('#singUp').click(cambio);
 
 function cambio() {
 	$('#container2').fadeOut(900);
-	$('#container3').show();
 }
 
 /*SING UP*/
@@ -18,15 +17,91 @@ function cambio() {
 	});
 
 /*DESHABILITAR EL BOTON*/
-	$('#next').click(function(){
+	$('#input').keypress(function() {
+		console.log("press");
+
+		var cantNum = $(this).val();
+		var lengthNum = cantNum.length + 1;
+
+		if (lengthNum ==  10) {
+			$('#next1').removeAttr('disabled');
+		} else {
+			$('#next1').attr('disabled', 'disabled');
+		}
+  });
+
+	$('#container4').hide();
+	$('#container5').hide();
+	$('#container6').hide();
+
+	$('#next').click(function(next){
 		alert('Tú código: LAB-551');
 
-		$('#container3').fadeOut(800);
+	$('#container3').fadeOut(700);
 	})
 
+		$(next).click(function(){
+			$('#container4').fadeIn(700);
+		})
+
+/*
+*CONTAINER 4
+*VERIFICANDO
+*/
+
+	$('#codigo1').keypress(function() {
+		console.log("press");
+
+		var cantNum = $(this).val();
+		var lengthNum = cantNum.length + 1;
+
+		if (lengthNum ==  3) {
+			$('#boton').removeAttr('disabled');
+		} else {
+			$('#boton').attr('disabled', 'disabled');
+		}
+  });
+
+/*
+*CAMBIANDO PAGINA
+*/
+
+	$('#boton').click(function(next){
+		$('#container4').fadeOut(700);
+	})
+
+ 	$(next).click(function(){
+ 			$('#container6').hide();
+			$('#container5').show();
+		})
 
 
+	/*CONTAINER 5*/
 
+
+/*
+*HABILITANDO EL BOTON CON EL CHECKBOX
+*/
+
+	$('#check').click(function() {
+		if ($(this).is(':checked')) {
+			$('#btn3').removeAttr('disabled');
+		} else {
+			$('#btn3').attr('disabled', 'disabled');
+		}
+  });
+
+/*
+* ULTIMA PAGINA
+*/
+
+	$('#btn3').click(function(next){
+		$('#container5').fadeOut(700);
+	})
+
+	$(next).click(function(){
+		$('#container6').fadeIn(700);
+	})
 
 
 
